359562ff49013f6648ec933736b79508a24bbfa7
tztsai <1213815284@qq.com>
2024-11-11 14:02:43 +0000

    Refactor build script and update manifest files

--- build/readability/build.sh
+++ build/readability/build.sh
@@ -5,14 +5,15 @@ cd "$(dirname "$0")"
 
 # before
 npm ci 2> /dev/null || npm i
-mkdir -p tmp
+# mkdir -p tmp
 
 # readability.min.js
-cp node_modules/@mozilla/readability/Readability.js tmp/readability.js
-npx terser --compress --mangle -- tmp/readability.js > tmp/readability.min.js
+# cp node_modules/@mozilla/readability/Readability.js tmp/readability.js
+# npx terser --compress --mangle -- tmp/readability.js > tmp/readability.min.js
 
 # copy
-cp tmp/readability.min.js ../../vendor/
+# cp tmp/readability.min.js ../../vendor/
+mv node_modules/@mozilla/readability/Readability.js ../../vendor/readability.min.js
 
 # after
-rm -rf node_modules/ tmp/
+rm -rf node_modules/
