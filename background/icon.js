0020320d3f190ede637de99efa26a55b0d75a14e
tztsai <1213815284@qq.com>
2024-11-13 14:53:23 +0000

    Update icon sizes and add new icons for light and dark themes

--- background/icon.js
+++ background/icon.js
@@ -3,7 +3,7 @@ md.icon = ({storage: {state}}) => () => {
 
   setTimeout(() =>
     chrome.action.setIcon({
-      path: [16, 19, 38, 48, 128].reduce((all, size) => (
+      path: [16, 32, 64].reduce((all, size) => (
         all[size] = `/icons/${state.settings.icon}/${size}x${size}.png`,
         all
       ), {})
